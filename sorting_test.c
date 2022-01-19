fed8c88013d9fee41513bb0c80ed43d1485bbc65
xDec0de_ <63256529+xdec0de@users.noreply.github.com>
2022-01-19 18:00:19 +0100

    Sorting 2 & 3 numbers (FINALLY!!)

--- sorting_test.c
+++ sorting_test.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 14:29:49 by danimart          #+#    #+#             */
-/*   Updated: 2022/01/19 17:36:32 by danimart         ###   ########.fr       */
+/*   Updated: 2022/01/19 17:58:55 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	test_sort(t_list **a, t_list **b, int size)
 {
+	if (size == 2)
+		swap_a(a, 1);
 	if (size == 3)
 		sort_three(a, b);
 }
@@ -27,13 +29,22 @@ void	sort_three(t_list **a, t_list **b)
 	f = (*a)->content;
 	s = (*a)->next->content;
 	t = (*a)->next->next->content;
-	if(*b)
+	if (*b)
 		printf("");
-	if(f > s && s < t && f < t)
+	if (f > s && s < t && f < t)
 		swap_a(a, 1);
-	else if(f > s && s > t)
+	else if (f > s && s > t && t < f)
 	{
 		swap_a(a, 1);
 		rev_rotate_a(a, 1);
 	}
+	else if (f > s && s < t && f > t)
+		rotate_a(a, 1);
+	else if (f < s && s > t && t > f)
+	{
+		swap_a(a, 1);
+		rotate_a(a, 1);
+	}
+	else
+		rev_rotate_a(a, 1);
 }
